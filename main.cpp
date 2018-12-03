7c6b72f2e5c5f357a9db657886481a933f0ccd71
lqh <1154546529@qq.com>
2018-12-03 21:15:07 +0800

    reset all

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
 
-    return a.exec();
+//    return a.exec();
+//}
+
+int main()
+{
+    return 0;
 }
